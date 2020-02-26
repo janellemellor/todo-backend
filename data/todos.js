6d66f38205dfff4dcf7884fe710929ce1baded3e
janelle mellor <janellemellor@gmail.com>
2020-02-26 09:59:56 -0800

    add data files

---
+++ data/todos.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+        task: 'drink water',
+        complete: true,
+    },
+    {
+        task: 'go on a hike',
+        complete: true,
+    },
+    {
+        task: 'do yoga',
+        complete: false,
+    },
+
+];
