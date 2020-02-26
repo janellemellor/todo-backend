7554920c84c7e47a2a5a30d741c4f117009dd6c9
janelle mellor <janellemellor@gmail.com>
2020-02-26 11:43:53 -0800

    fix dotenv file connection and get route

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+// (add cors, pg, and morgan...)
+const cors = require('cors');
+const morgan = require('morgan');
+const pg = require('pg');
+const server = express();
+
+console.log(process.env);
+// Database Client
+const Client = pg.Client;
+// (create and connect using DATABASE_URL)
+const client = new Client(process.env.DATABASE_URL);
+// console.log('==============', process.env.DATABASE_URL);
+client.connect();
+
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+// (add middleware utils: logging, cors, static files from public)
+// app.use(...)
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//get route to todos API
+app.get('/api/todos', async(req, res) => {
+    try {
+        const result = await client.query(`
+        SELECT * FROM todos;
+        
+        `);
+        res.json(result.rows);
+        console.log(result.rows);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+//start the server
+app.listen(PORT, () => {
+    console.log('server running PORT', PORT);
+});
+
+//set up export for testing 
+module.exports = { server: server };
